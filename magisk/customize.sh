0f983b96ac8e8f247cd998f1241672bbd6ecb034
cnage <52753181+cnage@users.noreply.github.com>
2022-03-18 10:56:10 +0800

    Update customize.sh

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -1,12 +1,54 @@
-#!/system/bin/sh
+##########################################################################################
+#
+# Magisk模块自定义安装脚本
+#
+##########################################################################################
 
-# Android 8.0 or newer
-if [[ "$(getprop ro.build.version.sdk)" -lt 26 ]]; then
-    ui_print "This module only supports Android 8.0 and newer."
 
-    # Abort install and clean up
-    rm -fr "$TMPDIR" "$MODPATH"
-    exit 1
-fi
+##########################################################################################
+# 替换列表
+##########################################################################################
 
-chmod 755 "$MODPATH/service.sh" "$MODPATH/post-fs-data.sh"
+# 列出要在系统中直接替换的所有目录
+# 您可以在变量名中声明要直接替换的文件夹列表REPLACE。模块安装程序脚本将提取此变量并为您创建文件.replace。
+
+# 按以下格式构建替换列表
+# 示例
+REPLACE_EXAMPLE="
+/system/app/YouTube
+/system/app/Bloatware
+"
+#上面的替换列表将导致创建以下文件：
+#$MODPATH/system/app/YouTube/.replace
+#$MODPATH/system/app/Bloatware/.replace
+
+# 在这里构建自定义替换列表
+REPLACE="
+"
+
+##########################################################################################
+# Permissions
+##########################################################################################
+
+set_permissions() {
+  : # Remove this if adding to this function
+
+  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
+  # Some examples:
+  
+  # For directories (includes files in them):
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  
+  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
+  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
+
+  # For files (not in directories taken care of above)
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  
+  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
+  # set_perm /data/local/tmp/file.txt 0 0 644
+}
+
+##########################################################################################
+# MMT Extended Logic - Don't modify anything after this
+##########################################################################################
